b828660c36baead69a0727ff9155be3a71ab33f7
sparsh-exe <sparshjain2607@gmail.com>
2026-08-20 16:05:17 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 24 MB (32.92%)

---
+++ 3347-distribute-elements-into-two-arrays-i/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums) {
+        vector<int> arr1 = {nums[0]};
+        vector<int> arr2 = {nums[1]};
+        int i;
+        for(i=2;i<nums.size();i++){
+            if(arr1[arr1.size()-1]>arr2[arr2.size()-1])
+                arr1.push_back(nums[i]);
+            else
+                arr2.push_back(nums[i]);
+        }
+        for(int i = 0;i<arr2.size();i++){
+            arr1.push_back(arr2[i]);
+        }
+        return arr1;
+    }
+};

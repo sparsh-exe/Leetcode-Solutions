e72531bfd860de606cc57fea3b76eb7a4ea221be
sparsh-exe <sparshjain2607@gmail.com>
2026-09-15 16:29:24 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 21.8 MB (46.87%)

---
+++ 0057-insert-interval/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> ans;
+        int i = 0;
+        int n = intervals.size();
+        //Intervals before new
+        while(i < n && intervals[i][1] < newInterval[0]) {
+            ans.push_back(intervals[i]);
+            i++;
+        }
+        //Overlapping intervals
+        while(i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        ans.push_back(newInterval);
+        //Intervals after new
+        while(i < n) {
+            ans.push_back(intervals[i]);
+            i++;
+        }
+        return ans;
+    }
+};

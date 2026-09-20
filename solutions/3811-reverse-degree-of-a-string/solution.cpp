b22219beb8e984cb0d5b40b96d7e1f7b84371e51
sparsh-exe <sparshjain2607@gmail.com>
2026-09-20 14:03:15 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.7 MB (78.56%)

---
+++ solutions/3811-reverse-degree-of-a-string/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int sum = 0;
+        for(int i = 0; i<s.size(); i++){
+            sum = sum + (('z' - s[i] + 1) * (i+1));
+        }
+        return sum;
+    }
+};
